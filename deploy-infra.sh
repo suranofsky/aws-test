f21d1ba8b1fb34f4d7729aee7194aedb61c4fde2
EC2 Default User <ec2-user@ip-172-31-27-132.ec2.internal>
2021-05-26 13:33:04 +0000

    add codebuild /codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,7 +6,20 @@ CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro
 
-#Deploy the CloudFormation template
+#Deploys static resources
+echo -e "\n\n================= Deploying setup.yml==========="
+aws cloudformation deploy \
+	--region $REGION \
+	--profile $CLI_PROFILE \
+	--stack-name $STACK_NAME-setup \
+	--template-file setup.yml \
+	--no-fail-on-empty-changeset \
+	--capabilities CAPABILITY_NAMED_IAN \
+	--parameter-overrides \
+	CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+
+# Deploy the CloudFormation template
 echo -e "\n\n==========Deploying main.yml======="
 aws cloudformation deploy \
 	--region $REGION \
---
+++ start-service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PID=0
+if [ -f './pid' ]; then
+	    PID=$(cat ./pid)
+fi
+
+if ps -p $PID > /dev/null
+then
+	   echo "Service with $PID is already running"
+   else
+	       nohup java -jar ./app/target/aws-test-fat.jar > service.out 2> errors.txt < /dev/null & PID=$!; echo $PID > ./pid
+fi
