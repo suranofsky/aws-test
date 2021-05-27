560fc8f99ab82eb90af33ce3fb909620ed6d5071
suranofsky <50344679+suranofsky@users.noreply.github.com>
2021-05-27 17:00:12 -0400

    test

--- stop-service.sh
+++ stop-service.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-if [ -f './pid' ]; then
-	    PID=$(cat ./pid) && kill -15 $PID
-fi
+#Check if the program is running
+is_exist(){
+  pid=`ps -ef|grep $APP_NAME|grep -v grep|awk '{print $2}' `
+     #If there is no return, there is a return of 0.     
+  if [ -z "${pid}" ]; then
+   return 1
+  else
+    return 0
+  fi
+}
+
+
+  is_exist
+  if [ $? -eq "0" ]; then
+    kill -9 $pid
+  else
+    echo "${APP_NAME} is not running"
+  fi  
