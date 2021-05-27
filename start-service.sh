faa8a7c68e8f0067f6a5775ee839b2bf6d9d4a6f
suranofsky <50344679+suranofsky@users.noreply.github.com>
2021-05-27 17:00:17 -0400

    test

--- start-service.sh
+++ start-service.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-PID=0
-if [ -f './pid' ]; then
-	    PID=$(cat ./pid)
-fi
+ #Check if the program is running
+is_exist(){
+  pid=`ps -ef|grep $APP_NAME|grep -v grep|awk '{print $2}' `
+     #If there is no return, there is a return of 0.     
+  if [ -z "${pid}" ]; then
+   return 1
+  else
+    return 0
+  fi
+}
 
-if ps -p $PID > /dev/null
-then
-	   echo "Service with $PID is already running"
-   else
-	       nohup java -jar ./app/target/aws-test-fat.jar > service.out 2> errors.txt < /dev/null & PID=$!; echo $PID > ./pid
-fi
+
+is_exist
+  if [ $? -eq "0" ]; then
+    echo "${APP_NAME} is already running. pid=${pid} ."
+  else
+    nohup java -jar ./app/target/aws-test-fat.jar > /dev/null 2>&1 &
+  fi
